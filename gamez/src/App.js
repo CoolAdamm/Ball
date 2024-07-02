9b2242eb89190f9eeee1f3e6770bbe50648eb350
Isak <isakflemming@MBPsomtlhorIsak.lan>
2024-07-02 13:28:39 +0200

    HEjsan

---
+++ gamez/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+      Sick nostalgiska gamez
+      </p>
+    </div>
+  );
+}
+
+export default App;
